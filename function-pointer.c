a84b56ee7c933c13fadcbdcfef269b9b947fafdf
biningo <biningo@yeah.net>
2020-08-24 22:41:12 +0800

    function pointer

---
+++ function-pointer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//函数指针
+int max(int one,int second){
+    return one>second?one:second;
+}
+int main(int argc, char const *argv[])
+{
+    int (*maxP)(int one,int second);
+    maxP = max;
+    printf("%d\n",maxP(1,2));  
+    return 0;
+}
